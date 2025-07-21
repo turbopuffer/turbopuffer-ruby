5d1aa92fb0a17331a4c228b01301581601767d22
Nikhil Benesch <nikhil.benesch@gmail.com>
2025-07-21 12:48:06 -0400

    fix: formatting

--- test/turbopuffer/custom_test.rb
+++ test/turbopuffer/custom_test.rb
@@ -4,18 +4,18 @@ require "securerandom"
 require_relative "test_helper"
 
 class Turbopuffer::Test::CustomTest < Minitest::Test
-    def before_all
-        super
-        @turbopuffer = Turbopuffer::Client.new(region: "gcp-us-central1")
-    end
+  def before_all
+    super
+    @turbopuffer = Turbopuffer::Client.new(region: "gcp-us-central1")
+  end
 
-    def setup
-        super
-        @ns = @turbopuffer.namespace("test-rb-#{SecureRandom.uuid}")
-    end
+  def setup
+    super
+    @ns = @turbopuffer.namespace("test-rb-#{SecureRandom.uuid}")
+  end
 
-    def test_smoke
-        result = @ns.write(upsert_rows: [{id: 1, vector: [1, 2, 3]}])
-        assert_equal(result.rows_affected, 1)
-    end
+  def test_smoke
+    result = @ns.write(upsert_rows: [{id: 1, vector: [1, 2, 3]}])
+    assert_equal(result.rows_affected, 1)
+  end
 end
